ccf1f02e4b266fecab6116cfed7e46893c236143
AndreySchmidt <ringnull@gmail.com>
2022-01-04 04:42:42 +0300

    fix

--- src/modules/blog/reducers.js
+++ src/modules/blog/reducers.js
@@ -1,18 +1,18 @@
-import { LOAD_ARTICLE_LIST_SUCCESSED_ACTION, LOAD_ARTICLE_LIST_ACTION, QUANTITY_CHANGED_ACTION } from './constants'
+import { LOAD_BLOG_LIST_ACTION, LOAD_BLOG_LIST_POPULAR_ACTION, LOAD_BLOG_LIST_MOST_TALKED_ABOUT_ACTION } from './../../constants'
 export default function reducer (state = [], action) {
   switch (action.type) {
-    case LOAD_ARTICLE_LIST_SUCCESSED_ACTION:
+    case LOAD_BLOG_LIST_ACTION:
       return {
         list : [...action.payload.list],
         loaded: true,
         activePage: 0,
         // activeArticleList: this.getActiveArticles(articleList)
       }
-    case LOAD_ARTICLE_LIST_ACTION:
+    case LOAD_BLOG_LIST_POPULAR_ACTION:
       return {
         loaded: false,
       }
-    case QUANTITY_CHANGED_ACTION:
+    case LOAD_BLOG_LIST_MOST_TALKED_ABOUT_ACTION:
       return {
         quantity: Number(action.payload.quantity),
       }
