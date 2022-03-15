23b9ce533612f54b2387a5b8717b6aff74d3363a
AndreySchmidt <ringnull@gmail.com>
2022-03-16 02:30:48 +0300

    status

--- src/modules/personalAccount/pages/UserPage/components/Status/Status.js
+++ src/modules/personalAccount/pages/UserPage/components/Status/Status.js
@@ -2,19 +2,36 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Status = (props) => {
+
+  const state = {
+    status: {
+      editMode: false,
+    }
+  }
+
+  const activateEditMode = () => {
+    // state.status.editMode = true
+    // debugger
+  }
+
+  const deactivateEditMode = () => {
+
+  }
+
   return (
     <div className="status_text">
-      {props.status}
-      &nbsp;&nbsp;
+      <span onDoubleClick = { activateEditMode }>{ props.status }&nbsp;&nbsp;</span>
       <Link className="change_status" to="">изменить статус</Link>
 
-      <div className="c_change_status_form">
-        <textarea className="c_post_comment_area" value={props.status}></textarea>
+      { state.status.editMode &&
+        <div onBlur = { deactivateEditMode } className="c_change_status_form">
+          <textarea className="c_post_comment_area" value={ props.status }></textarea>
 
-        <div className="save_button_wrapper">
-          <input className="c_status_button" type="submit" value="Сохранить"/>
+          <div className="save_button_wrapper">
+            <input className="c_status_button" type="submit" value="Сохранить"/>
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
